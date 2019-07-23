2316e7ed4fd358e5ff39cc30d6672134835c07d6
Kyle Walker <kwalker@redhat.com>
2019-07-23 16:37:46 -0400

    setup: Change the formatting for RPM compatibility
    Thre previous revision was exceptionally verbose and didn't play well with
    the "python setup.py bdist_rpm" process. The new revision makes it more
    terse.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-DESCRIPTION = """
+LONG_DESCRIPTION = """
 Leverage Red Hat's Security Data API to find CVEs by various attributes
 (date, severity, scores, package, IAVA, etc). Retrieve customizable details
 about found CVEs or about specific CVE ids input on cmdline. Parse
@@ -14,7 +14,7 @@ instantaneous http request and CVE retrieval is parallelized, utilizing
 multiple threads at once. Python requests is used for all remote
 communication, so proxy support is baked right in. BASH intelligent
 tab-completion is supported via optional Python argcomplete module. Python2
-tested on RHEL6, RHEL7, & Fedora and Python3 on Fedora but since it doesn't
+tested on RHEL6, RHEL7, & Fedora and Python3 on Fedora but since it doesnt
 integrate with RHN/RHSM/yum/Satellite, it can be used on any
 internet-connected machine. Feedback, feature requests, and code
 contributions welcome.
@@ -24,7 +24,7 @@ setup(
     version = '1.0.1',
     author = 'Ryan Sawhill Aroha',
     author_email = 'rsaw@redhat.com',
-    description = (DESCRIPTION),
+    description = 'Provides a simple interface for the Red Hat Security Data API',
     license = 'GPL',
     packages = find_packages(),
 
@@ -33,7 +33,7 @@ setup(
         'requests',
     ],
 
-    long_description=read('README.md')
+    long_description=LONG_DESCRIPTION
 )
 
 
